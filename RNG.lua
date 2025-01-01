e44ea89e000fd31f36fd2671a4dfab95b9df2d5c
halfwit <michaelmisch1985@gmail.com>
2025-01-01 12:56:41 -0800

    Update RNG.lua

--- RNG.lua
+++ RNG.lua
@@ -2,55 +2,43 @@ local profile = {};
 gcinclude = gFunc.LoadFile('common\\include.lua');
 
 local actions = {
-
+    ['Slug Shot']     = 'Livin\' that slug life',
+    ['Sidewinder']    = 'Go go gadget Sidewinder!',
+    ['Velocity Shot'] = 'MOAR POWER!',
 };
 
 local sets = {
-    -- Haste == Attack here, useful when sharpshot overcaps us
-    ['TpHaste'] = {
-        Head = 'Noct Beret +1',
-        Neck = 'Peacock Amulet',
+    ['StrBalanced'] = {
+        Head = 'Hunter\'s Beret',
+        Neck = 'Spike Necklace',
         Ear1 = 'Genin Earring',
         Ear2 = 'Drone Earring',
-        Body = 'Noct Doublet +1',
+        Body = 'Shikaree Aketon',
         Hands = 'Noct Gloves +1',
-        Ring1 = 'Beetle Ring +1',
-        --Ring2 = 'Beetle Ring +1',
+        Ring1 = 'Marksman\'s Ring',
         Ring2 = 'Rajas Ring',
-        Waist = 'Brave Belt',
+        Waist = 'Ryl.Kgt. Belt',
         Back = 'Nomad\'s Mantle',
         Legs = 'Noct Brais +1',
         Feet = 'Savage Gaiters',
     },
     ['TpBalanced'] = {
-        Head = 'Noct Beret +1',
+        Head = 'Hunter\'s beret',
         Neck = 'Peacock Amulet',
         Ear1 = 'Genin Earring',
         Ear2 = 'Drone Earring',
-        Body = 'Noct Doublet +1',
+        Body = 'Hunter\'s jerkin',
         Hands = 'Noct Gloves +1',
-        Ring1 = 'Beetle Ring +1',
-        --Ring2 = 'Beetle Ring +1',
+        Ring1 = 'Marksman\'s Ring',
         Ring2 = 'Rajas Ring',
-        Waist = 'Brave Belt',
+        Waist = 'Ryl.Kgt. Belt',
         Back = 'Nomad\'s Mantle',
         Legs = 'Noct Brais +1',
         Feet = 'Savage Gaiters',
     },
+    ['TpHaste'] = {
+    },
     ['TpAccuracy'] = {
-        Head = 'Noct Beret +1',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Genin Earring',
-        Ear2 = 'Drone Earring',
-        Body = 'Noct Doublet +1',
-        Hands = 'Noct Gloves +1',
-        Ring1 = 'Beetle Ring +1',
-        --Ring2 = 'Beetle Ring +1',
-        Ring2 = 'Rajas Ring',
-        Waist = 'Brave Belt',
-        Back = 'Nomad\'s Mantle',
-        Legs = 'Noct Brais +1',
-        Feet = 'Savage Gaiters',
     },
     ['TpEvasion'] = {
         -- Actual evasion set wip
@@ -58,23 +46,9 @@ local sets = {
     ['Kiting'] = {
         -- Same as evasion, with movement speed
     },
-    ['WsAccuracy'] = {
-        Head = 'Noct Beret +1',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Genin Earring',
-        Ear2 = 'Optical Earring',
-        -- Ear2 = 'Drone Earring',
-        Body = 'Noct Doublet +1',
-        Hands = 'Noct Gloves +1',
-        Ring1 = 'Beetle Ring +1',
-        Ring2 = 'Beetle Ring +1',
-        Waist = 'Brave Belt',
-        Back = 'Nomad\'s Mantle',
-        Legs = 'Noct Brais +1',
-        Feet = 'Savage Gaiters',
-    },
-    ['WsDmg'] = {
-
+    ['Emnity'] = {
+        Body = 'Shikaree Aketon',
+        Head = 'Pumpkin Head'
     }
 };
 
@@ -91,6 +65,7 @@ end
 profile.OnLoad = function()
     gSettings.AllowAddSet = true;
     gcinclude.Initialize();
+    gcinclude.TpVariant = 3;
     profile.Macros();
 end
 
@@ -105,8 +80,13 @@ end
 
 profile.HandleDefault = function()
     local player = gData.GetPlayer();
+    local ss = gData.GetBuffCount('Sharpshot');
     if (player.Status == 'Engaged') then
-        gFunc.EquipSet('Tp' .. gcinclude.TpVariantTable[gcinclude.TpVariant]);
+        if(ss  == 1) then
+            gFunc.EquipSet('Str' .. gcinclude.TpVariantTable[gcinclude.TpVariant]);
+        else
+            gFunc.EquipSet('Tp' .. gcinclude.TpVariantTable[gcinclude.TpVariant]);
+        end
     elseif (player.Status == 'Resting') then
         gFunc.EquipSet(sets.Resting);
     else
@@ -116,27 +96,42 @@ profile.HandleDefault = function()
 end
 
 profile.HandleAbility = function()
+    local action = gData.GetAction();
+    -- Minus emnity anywhere we don't do DPS
+    gFunc.EquipSet('Emnity');
+    if (action.Name == 'Camouflage') then
+        gFunc.Equip('Body', 'Hunter\'s Jerkin');
+    elseif (action.Name == 'Scavenge') then
+        gFunc.Equip('Feet', 'Hunter\'s Socks');
+    elseif(action.Name == 'Shadowbind') then
+        gFunc.Equip('Hands', 'Hunter\'s Bracers');
+    elseif(action.Name == 'Sharpshot') then
+        gFunc.Equip('Legs', 'Hunter\'s Braccae');
+    end
+    gcinclude.TelegraphAction(actions, action.Name);
 end
 
 profile.HandleItem = function()
 end
 
+-- Before we cast a spell like utsu
 profile.HandlePrecast = function()
+    gFunc.EquipSet('Emnity');
 end
 
 profile.HandleMidcast = function()
+    gFunc.EquipSet(sets.TpEvasion);
 end
 
 profile.HandlePreshot = function()
+    -- Check if we can Unlimited shot?
 end
 
 profile.HandleMidshot = function()
 end
 
 profile.HandleWeaponskill = function()
-    -- Check for Sharpshot acc boost
-    -- If so, use the higher str set
-    gFunc.EquipSet(sets.WsAccuracy);
+    gcinclude.TelegraphAction(actions, ws.Name);
 end
 
 return profile;
